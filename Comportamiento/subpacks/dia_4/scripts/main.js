16edecbbe2303b2093ad2a326fe1bfa40885c676
convex <salute.sabe@outlook.com>
2022-05-24 22:57:41 -0500

    Update main.js

--- Comportamiento/subpacks/dia_4/scripts/main.js
+++ Comportamiento/subpacks/dia_4/scripts/main.js
@@ -3,28 +3,80 @@ GitHubs: https://github.com/CibNumeritos https://github.com/conveex
 *********************************************************************
 Este archivo pertenece a: "conveex" (Ha Juegos Copyright 2022), Cualquier modificación o cambio no autorizado será sancionado, más información para autorizar tus copias puedes contactar conmigo en discord: https://discord.gg/p6a7tqVJxn
 Sus GitHubs: https://github.com/conveex */
-import{EntityQueryOptions,EntityQueryScoreOptions,world}from"mojang-minecraft"
-const ban=new EntityQueryScoreOptions()
-ban.objective="ban"
-ban.minScore=1
-const banQuery=new EntityQueryOptions
-banQuery.scoreOptions=[ban]
-world.events.beforeChat.subscribe(eventData=>{eventData.cancel=true
-const msg=eventData.message
-const player=eventData.sender
-world.getDimension("overworld").runCommand(`tellraw @a {"rawtext":[{"text":"§8§l[§r${(player.getTags().find((tag) => tag.startsWith("r:"))?.substring(2)?.split("-") ?? ["§4Sobreviviente"]).join("§r§l§8][§r")}§8§l]§r ${player.name} §8§l>>§r ${msg}"}]}`)
-if(player.hasTag("Admin")){const args=msg.trim().split(/\s+/)
-if(msg.startsWith("!ban")){eventData.cancel=true
-player.runCommand(`scoreboard players set ${args[1]} ban 1`)}
-if(msg.startsWith("!unban")){eventData.cancel=true
-player.runCommand(`scoreboard players set ${args[1]} ban 0`)}}else{}})
-world.events.tick.subscribe(()=>{const players=Array.from(world.getPlayers(banQuery))
-for(const player of players){player.runCommand(`kick ${player.name} ¡Has muerto y ahora estas baneado!`)}})
-world.events.tick.subscribe(eventKit=>{try{for(const player of world.getPlayers()){if(!player.hasTag("Kit")){world.getDimension("overworld").runCommand(`execute "${player.nameTag}" ~ ~ ~ replaceitem entity @s slot.weapon.offhand 0 totem`)
-world.getDimension("overworld").runCommand(`execute "${player.nameTag}" ~ ~ ~ replaceitem entity @s slot.hotbar 8 golden_carrot 15`)
-world.getDimension("overworld").runCommand(`execute "${player.nameTag}" ~ ~ ~ replaceitem entity @s slot.hotbar 7 water_bucket`)
-world.getDimension("overworld").runCommand(`execute "${player.nameTag}" ~ ~ ~ scoreboard objectives add ban dummy ban`)
-player.addTag("Kit")}else{}}}catch{}})
+
+import { EntityQueryOptions, EntityQueryScoreOptions, world } from "mojang-minecraft"
+const ban = new EntityQueryScoreOptions()
+ban.objective = "ban"
+ban.minScore = 1
+const banQuery = new EntityQueryOptions
+banQuery.scoreOptions = [ban]
+world.events.beforeChat.subscribe(eventData => {
+    eventData.cancel = true
+    const msg = eventData.message
+    const player = eventData.sender
+    world.getDimension("overworld").runCommand(`tellraw @a {"rawtext":[{"text":"§8§l[§r${(player.getTags().find((tag) => tag.startsWith("r:"))?.substring(2)?.split("-") ?? ["§4Sobreviviente"]).join("§r§l§8][§r")}§8§l]§r ${player.name} §8§l>>§r ${msg}"}]}`)
+    if (player.hasTag("Admin")) {
+        const args = msg.trim().split(/\s+/)
+        if (msg.startsWith("!ban")) {
+            eventData.cancel = true
+            player.runCommand(`scoreboard players set ${args[1]} ban 1`)
+        }
+        if (msg.startsWith("!unban")) {
+            eventData.cancel = true
+            player.runCommand(`scoreboard players set ${args[1]} ban 0`)
+        }
+    } else { }
+})
+world.events.tick.subscribe(() => {
+    const players = Array.from(world.getPlayers(banQuery))
+    for (const player of players) { player.runCommand(`kick ${player.name} ¡Has muerto y ahora estas baneado!`) }
+})
+world.events.tick.subscribe(eventKit => {
+    try {
+        for (const player of world.getPlayers()) {
+            if (!player.hasTag("Kit")) {
+                world.getDimension("overworld").runCommand(`execute "${player.nameTag}" ~ ~ ~ replaceitem entity @s slot.weapon.offhand 0 totem`)
+                world.getDimension("overworld").runCommand(`execute "${player.nameTag}" ~ ~ ~ replaceitem entity @s slot.hotbar 8 golden_carrot 15`)
+                world.getDimension("overworld").runCommand(`execute "${player.nameTag}" ~ ~ ~ replaceitem entity @s slot.hotbar 7 water_bucket`)
+                world.getDimension("overworld").runCommand(`execute "${player.nameTag}" ~ ~ ~ scoreboard objectives add ban dummy ban`)
+                player.addTag("Kit")
+            } else { }
+        }
+    } catch { }
+})
+
+world.events.beforeItemUse.subscribe(eventMilk => {
+    const player = eventMilk.source
+    const item = eventMilk.item
+    if (item.id == 'minecraft:milk_bucket') {
+            player.addTag("TomoLeche")
+    }
+})
+
+world.events.tick.subscribe(eventEffect => {
+    try {
+        for (const plr of world.getPlayers()) {
+            if (runCommand(`execute "${plr.nameTag}" ~ ~ ~ testfor @s[hasitem={location=slot.weapon.mainhand,item=bucket}]`).error == false && plr.hasTag("TomoLeche")) {
+                world.getDimension("overworld").runCommand(`effect "${plr.nameTag}" nausea 10 0 false`)
+                world.getDimension("overworld").runCommand(`effect "${plr.nameTag}" poison 10 9 false`)
+                plr.removeTag("TomoLeche")
+            }
+        }
+    } catch { }
+})
+
+function runCommand(command) {
+    try {
+        return {
+            error: false, ...world.getDimension("overworld").runCommand(command)
+        }
+    } catch (error) {
+        return {
+            error: true
+        }
+    }
+}
+
 /* This file belongs to: "conveex" (Ha Juegos Copyright 2022), Any unauthorized modification or change will be penalized, more information to authorize your copies you can contact me on discord: https://discord.gg/p6a7tqVJxn
 GitHubs: https://github.com/CibNumeritos https://github.com/conveex
 *********************************************************************
